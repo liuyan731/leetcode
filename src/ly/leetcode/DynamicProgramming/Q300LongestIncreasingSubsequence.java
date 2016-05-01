2823fa81d790164b7a73dfefff18c382b30582cc
liuyan <liuyan990731@gmail.com>
2016-05-01 22:45:44 +0800

    5.1日只补充了一道LCSequence的问题

--- src/ly/leetcode/DynamicProgramming/Q300LongestIncreasingSubsequence.java
+++ src/ly/leetcode/DynamicProgramming/Q300LongestIncreasingSubsequence.java
@@ -8,11 +8,25 @@
 package ly.leetcode.DynamicProgramming;
 
 public class Q300LongestIncreasingSubsequence {
-	 public int lengthOfLIS(int[] nums) {
-		 
-	 }
+	public static int lengthOfLIS(int[] nums) {
+		int dp[] = new int[nums.length];
+		int maxLen = 0;
+		for (int i = 0; i < nums.length; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if ((nums[j] < nums[i]) && (dp[i] < dp[j] + 1)) {
+					dp[i] = dp[j] + 1;
+				}
+			}
+			if(dp[i] > maxLen) {
+				maxLen = dp[i];
+			}
+		}
+		return maxLen;
+	}
 
 	public static void main(String[] args) {
-		
+		int nums[] = {0};
+		System.out.println(lengthOfLIS(nums));
 	}
 }
