79632f506cc14465833595a292b3e93f649c9641
liuyan <liuyan990731@gmail.com>
2016-06-10 16:41:34 +0800

    6.10 afternoon

--- src/ly/leetcode/Math/Q119PascalTriangle2.java
+++ src/ly/leetcode/Math/Q119PascalTriangle2.java
@@ -11,30 +11,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Q119PascalTriangle2 {
-	
+
 	public static void main(String[] args) {
-		List<Integer> list = getRow(1);
+		List<Integer> list = getRow(4);
 		for (Integer integer : list) {
 			System.out.println(integer);
 		}
 	}
-	
+
 	public static List<Integer> getRow(int rowIndex) {
-		List<Integer> ret = new ArrayList<>();
-		rowIndex = rowIndex + 1;
-		if (rowIndex <= 2) {
-			for (int i = 0; i < rowIndex; i++) {
-				ret.add(1);
+		List<Integer> list = new ArrayList<Integer>();
+		if (rowIndex < 0)
+			return list;
+
+		for (int i = 0; i < rowIndex + 1; i++) {
+			list.add(0, 1);
+			for (int j = 1; j < list.size() - 1; j++) {
+				list.set(j, list.get(j) + list.get(j + 1));
 			}
-			return ret;
-		} else {
-			List<Integer> forwardRet = getRow(rowIndex - 1);
-			ret.add(1);
-			for (int i = 1; i < forwardRet.size(); i++) {
-				ret.add(forwardRet.get(i) + forwardRet.get(i - 1));
-			}
-			ret.add(1);
-			return ret;
 		}
+		return list;
 	}
+	
+	
 }
